9a9efdcac8bbf9cbb4759256866f9c3db603ed2b
Vitaly Ko <vitaly@lcs.lv>
2022-08-06 17:56:10 +0300

    initial commit

---
+++ src/main/java/com/example/gifcurrencyapp/GifCurrencyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.gifcurrencyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifCurrencyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifCurrencyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gifcurrencyapp/GifCurrencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gifcurrencyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifCurrencyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
